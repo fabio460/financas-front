d3b8ce4a5e61af3e951396997f68ba1d2d9c1e2e
Fabio da Silva de Oliveira <dasilvadeoliveirafabio@gmail.com>
2023-12-09 13:17:55 -0300

    ajuste tamanho do layout

--- src/Componentes/Home/Carousel/index.tsx
+++ src/Componentes/Home/Carousel/index.tsx
@@ -1,8 +1,9 @@
-import React,{useState} from 'react'
+import React,{useEffect, useRef, useState} from 'react'
 import "./carousel.css"
 import { mesType } from '../../../types'
 import Card from '@mui/material/Card';
 import DeleteIcon from '@mui/icons-material/Delete';
+import { Typography } from "@mui/material";
 import CardActions from '@mui/material/CardActions';
 import ArrowBackIosNewIcon from '@mui/icons-material/ArrowBackIosNew';
 import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
@@ -31,13 +32,42 @@ export default function Carousel({mes, handleAtualiza}:{mes:mesType[],handleAtua
     }
   }
   const sliderStyle = {
-    "marginLeft":`${-active*300}px`,
+    "marginLeft":`${-active*400}px`,
+    margin:"auto",
     "@media (max-width:400px)":{
-       "marginLeft":`${-active*300}px`
+      //  "marginLeft":`${-active*300}px`,
+       background:"red",
     }
+ 
   }
+    // Crie uma ref para a div
+    const [larguraDaDiv, setLarguraDaDiv] = useState<any>(null);
+    const minhaDivRef = useRef<any>(null);
+
+    useEffect(() => {
+      const atualizarLargura = () => {
+        if (minhaDivRef.current) {
+          // Acesse a largura usando offsetWidth
+          const largura = minhaDivRef.current?.offsetWidth;
+          setLarguraDaDiv(largura);
+        }
+      };
+  
+      // Chame a função ao montar o componente
+      atualizarLargura();
+  
+      // Adicione um ouvinte de redimensionamento para atualizar a largura se a janela for redimensionada
+      window.addEventListener('resize', atualizarLargura);
+  
+      // Remova o ouvinte de redimensionamento ao desmontar o componente
+      return () => {
+        window.removeEventListener('resize', atualizarLargura);
+      };
+    }, [])
+  
+
   return (
-    <div>
+    <div >
       <div className='slides'>
         <div style={{display:"flex", justifyContent:"center", alignItems:"center"}}>
           <IconButton sx={{color:corDosItens}} aria-label="delete" onClick={anterior} disabled={active === 0 ? true : false}>
@@ -49,7 +79,7 @@ export default function Carousel({mes, handleAtualiza}:{mes:mesType[],handleAtua
           </IconButton>
         </div>
         <div style={{textAlign:"center",marginTop:"20px"}}>Sobra {formatoMonetario(getSobra(mes[active]?.ganhos, mes[active]?.contas_A_Pagar))}</div>
-        <div className={`slideContainer`} style={sliderStyle}>
+        <div ref={minhaDivRef} className={`slideContainer`} style={sliderStyle}>
            { mes.length === 0 ? <div style={{display:"flex", justifyContent:"center", marginTop:"30px"}}>Não há dados</div>:
              mes.map((e,key)=>{
                return <div key={key} className='slide' style={{color:"black"}}>
